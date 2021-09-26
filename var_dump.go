3cbf740c28d0055c28e330c778cfe4d2eb673dcb
Daniel Leech <dan.t.leech@gmail.com>
2021-09-26 16:58:07 +0100

    Refactored

--- var_dump.go
+++ var_dump.go
@@ -1,34 +1,59 @@
 package dump
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
 
 type Dumper struct {
-    formatter formatter;
+    NumericFormat string
+    StringFormat string
 }
 
-func (d Dumper) ToString(value interface{}) string {
-    return d.formatter.Format(
-        loadData(
-            context{},
-            reflect.ValueOf(value),
-        ),
-    )
+func (f Dumper) ToString(value interface{}) string {
+    return f.dumpValue(reflect.ValueOf(value))
 }
 
-func loadData(ctx context, value reflect.Value) data {
-    return data{
-        value: value,
+func (f Dumper) dumpValue(value reflect.Value) string {
+    kind := value.Kind()
+    switch kind {
+    case reflect.Int:
+        return f.numeric(fmt.Sprintf("%d", value.Int()))
+    case reflect.Float32:
+        return f.numeric(fmt.Sprintf("%v", value.Float()))
+    case reflect.Float64:
+        return f.numeric(fmt.Sprintf("%v", value.Float()))
+    case reflect.String:
+        return f.string(fmt.Sprintf("%s", value.String()))
+    case reflect.Struct:
+        out := []string{}
+        for i := 0; i < value.NumField(); i++ {
+            field := value.Field(i);
+            formattedValue := f.dumpValue(
+                f.valueOfField(field),
+            )
+
+            out = append(out, fmt.Sprintf(
+                "%s:%s",
+                value.Type().Field(i).Name,
+                formattedValue,
+            ))
+        }
+        return fmt.Sprintf("%s{%s}", value.Type().Name(), strings.Join(out, " "))
     }
+    panic(fmt.Sprintf("Did not know how to format: %s", kind))
 }
 
-type formatter interface {
-    Format(data data) string;
+func (f Dumper) numeric(value string) string {
+    return fmt.Sprintf(f.NumericFormat, value)
 }
-
-type data struct {
-    value reflect.Value;
+func (f Dumper) string(value string) string {
+    return fmt.Sprintf(f.StringFormat, value)
 }
-
-type context struct {
-    depth int;
+func (f Dumper) valueOfField(v reflect.Value) reflect.Value {
+    if v.Kind() == reflect.Interface && !v.IsNil() {
+        return v.Elem()
+    }
+    return v
 }
---
+++ var_dump_test.go
@@ -0,0 +1,34 @@
+package dump
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type test1 struct {
+    Field1 string
+    Field2 int
+    private string
+}
+
+func TestDump(t *testing.T) {
+    t.Run("Scalars", func(t *testing.T) {
+        require.Equal(t, "n:12", testDump(12))
+        require.Equal(t, "n:12.123", testDump(12.123))
+        require.Equal(t, "s:hello", testDump("hello"))
+    })
+
+    t.Run("Structs", func(t *testing.T) {
+        s := test1{Field1: "hello", Field2: 23}
+        require.Equal(t, "test1{Field1:s:hello Field2:n:23 private:s:}", testDump(s))
+    })
+}
+
+func testDump(value interface{}) string {
+    dumper := Dumper{
+        NumericFormat: "n:%s",
+        StringFormat: "s:%s",
+    }
+    return dumper.ToString(value)
+}
